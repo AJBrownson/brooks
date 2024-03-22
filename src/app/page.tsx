1fae5e216fbe6402d611f1ee3ab0a337e16842f0
Anietie Brownson <anni.brownson@gmail.com>
2024-03-21 18:53:22 -0700

    feat: Add some mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Hero from "../../public/hero.svg";
+import HeroMobile from "../../public/hero-mobile.png"
 import About from "../../public/about-hero.png";
 import LockWhite from "../../public/lock-white.svg";
 import LockBlack from "../../public/lock-black.svg";
@@ -21,7 +22,8 @@ import VectorUp from "../../public/Vector 56.svg";
 import VectorDown from "../../public/vector-down.png";
 import Gaming from "../../public/gaming.png";
 import Logo from "../../public/logo.png";
-import "./custom.css";
+
+
 
 const strokeStyle = {
   textShadow:
@@ -32,21 +34,22 @@ export default function Home() {
   return (
     <main className="font-space relative">
       {/* Hero section */}
-      <Image src={Hero} alt="" className="w-full object-cover" id="home" />
-      <section className="z-50 absolute top-56 text-center px-8 lg:px-0">
+      <Image src={Hero} alt="" className="hidden lg:block w-full object-cover" id="home" />
+      <Image src={HeroMobile} alt="" className="w-full object-cover lg:hidden" />
+      <section className="z-50 absolute top-36 lg:top-56 text-center px-1 lg:px-0">
         <h1
-          className="mb-[-1.5rem] text-center text-[90px] lg:text-[164px] font-orbitron font-extrabold"
+          className="mb-[-1.5rem] text-center text-[72px] lg:text-[164px] font-orbitron font-extrabold"
           style={strokeStyle}
         >
           <span className="text-[#00D5E9]">R</span>oo
           <span className="text-[#00D5E9]">ks</span>
         </h1>
-        <p className="text-lg lg:px-72 font-orbitron">
+        <p className="text-sm lg:text-lg lg:px-72 font-orbitron">
           The most shareable meme coin out there. While the dogs and frogs have
           had their moment, now it's Rook's time to shine.
         </p>
-        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center lg:gap-3">
-          <button className="mt-4 lg:mt-8 px-3 lg:px-12 py-3 border border-white text-white hover:border-white relative group hover:border-gradient inline-flex items-center">
+        <div className="flex lg:items-center justify-center gap-3">
+          <button className="mt-4 lg:mt-8 px-8 lg:px-12 py-2 lg:py-3 border border-white text-white hover:border-white relative group hover:border-gradient inline-flex items-center">
             <span className="transition-opacity opacity-100 group-hover:opacity-0 absolute inset-0 flex items-center justify-center">
               Mint Rooks <Image src={LockWhite} alt="" className="ml-2" />
             </span>
@@ -56,7 +59,7 @@ export default function Home() {
             </span>
           </button>
 
-          <button className="font-light mt-4 lg:mt-8 px-3 lg:px-12 py-3 bg-[#0099FF] hover:bg-transparent border border-transparent hover:border-white relative group inline-flex items-center">
+          <button className="font-light mt-4 lg:mt-8 px-8 lg:px-12 py-2 lg:py-3 bg-[#0099FF] hover:bg-transparent border border-transparent hover:border-white relative group inline-flex items-center">
             <span className="transition-opacity opacity-100 group-hover:opacity-0 absolute inset-0 flex items-center justify-center">
               Presale <Image src={LockWhite} alt="" className="ml-2" />
             </span>
@@ -73,62 +76,26 @@ export default function Home() {
         <Image
           src={Gradient}
           alt="A gradient picture"
-          className="h-[21.5rem] absolute top-[12rem]"
+          className="lg:h-[21.5rem] absolute top-[9.5rem] lg:top-[12rem]"
         />
       </section>
 
       {/* About section */}
-      {/* h-72 sm:h-80 lg:h-96    h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128*/}
-      {/* <section className="mt-28 lg:mt-96 px-8 lg:px-40" id="about">
-
-      container flex justify-center mx-auto sm:py-12 lg:py-24 lg:justify-between
-        <div className="flex justify-between items-center">
-          <div className="">
-            <Image
-              src={About}
-              alt=""
-              className=""
-            />
-          </div>
-
-          <div className="flex flex-col w-[50%]">
-          <h1 className="font-orbitron font-bold lg:mb-10 leading-snug text-4xl">
-            ABOUT ROOKS
-          </h1>
-            <p className="mb-2 text-base">
-              Rooks Coin, the ultimate meme vibe! Just like rooks rock the
-              chessboard, we're here to rule the meme world! Our coin is all
-              about moving freely, spreading laughs in every direction, no
-              blocks allowed. We own the meme space, one viral trend or
-              hilarious post at a time, making sure the fun never ends. Rooks
-              Coin is the backbone of meme culture, and with us, you'll be
-              unstoppable in spreading the joy of memes!
-            </p>
-
-            <p className="font-medium text-lg">
-              Together, we'll make the digital world a more meme-friendly place,
-              one transaction at a time!
-            </p>
-          </div>
-        </div>
-      </section> */}
-
-      {/* About section */}
       <section
-        className="mt-28 lg:mt-96 px-8 lg:px-20 py-20 pb-36 gap-10"
+        className="mt-32 lg:mt-96 px-1 lg:px-20 py-20 lg:pb-36 gap-10"
         id="about"
       >
         <Image src={VectorUp} alt="" className="mb-10" />
-        <div className="flex items-center">
+        <div className="flex flex-col lg:flex-row items-center">
           <div className="">
             <Image src={About} alt="" className="" />
           </div>
 
-          <div className="flex flex-col w-[50%]">
-            <h1 className="font-orbitron font-bold lg:mb-5 leading-snug text-3xl">
+          <div className="flex flex-col text-center lg:text-left lg:w-[50%]">
+            <h1 className="font-orbitron font-bold mt-3 lg:mt-0 mb-5 leading-snug text-3xl">
               ABOUT ROOKS
             </h1>
-            <p className="mb-4 text-lg">
+            <p className="mb-4 text-base lg:text-lg">
               Rooks Coin, the ultimate meme vibe! Just like rooks rock the
               chessboard, we're here to rule the meme world! Our coin is all
               about moving freely, spreading laughs in every direction, no
@@ -138,7 +105,7 @@ export default function Home() {
               unstoppable in spreading the joy of memes!
             </p>
 
-            <p className="font-medium text-xl">
+            <p className="font-medium text-base lg:text-xl">
               Together, we'll make the digital world a more meme-friendly place,
               one transaction at a time!
             </p>
@@ -148,11 +115,11 @@ export default function Home() {
       </section>
 
       {/* Story section */}
-      <section className="py-10 px-8 lg:px-60 bg-small mt-20 lg:mt-0">
-        <h1 className="text-[#00D5E9] text-center font-extrabold text-2xl mb-5">
+      <section className="py-10 px-1 lg:px-60 bg-small">
+        <h1 className="text-[#00D5E9] text-center font-extrabold text-lg lg:text-2xl mb-5">
           Rooks Revolution: Shaping the Future of Memes
         </h1>
-        <p className="text-[#EDEDED] text-center text-lg mb-5">
+        <p className="text-[#EDEDED] text-center text-base lg:text-lg mb-5">
           In the tumultuous world of memecoins, a fierce battle ensues between
           "Frogs Coin" and "Dogs Coin," each touting their own symbols and
           promises. However, amidst the chaos, a new player quietly emerges:
@@ -165,12 +132,12 @@ export default function Home() {
       </section>
 
       {/* Carousel section */}
-      <section>
+      {/* <section>
         <InfiniteSlider />
-      </section>
+      </section> */}
 
       {/* Gaming section */}
-      <section className="mt-36 lg:px-20">
+      {/* <section className="mt-36 lg:px-20">
         <div className="py-6 px-8 flex items-center gap-60 bg-custom-gradient">
           <div className="flex flex-col">
             <h1 className="font-orbitron font-bold text-4xl leading-snug mb-3">
@@ -185,11 +152,11 @@ export default function Home() {
           </div>
           <Image src={Gaming} alt="" />
         </div>
-      </section>
+      </section> */}
 
       {/* Tokenomics section */}
       {/* lg:justify-between justify-center rounded-sm  xl:max-w-lg lg:text-left */}
-      <section className="px-8 lg:px-20 py-24" id="tokenomics">
+      {/* <section className="px-8 lg:px-20 py-24" id="tokenomics">
         <div className="container flex flex-col mx-auto sm:py-12 lg:py-24 lg:flex-row justify-between">
           <div className="flex flex-col justify-center lg:p-4 text-center lg:max-w-xl">
             <h1 className="text-center leading-snug text-4xl lg:text-5xl font-orbitron font-semibold">
@@ -218,10 +185,10 @@ export default function Home() {
             <Image src={Circle} alt="" />
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Footer section */}
-      <section className="bg-small text-center text-[#EDEDED] py-5 px-8 lg:px-64">
+      {/* <section className="bg-small text-center text-[#EDEDED] py-5 px-8 lg:px-64">
         <Image src={VectorUp} alt="" className="mb-16" />
         <h1 className="text-4xl lg:text-5xl font-orbitron font-extrabold">
           Contact Rooks
@@ -266,10 +233,10 @@ export default function Home() {
         <div className="font-orbitron font-medium text-xs lg:text-md">
           <p>2024 Rooks. All rights reserved!</p>
         </div>
-      </section>
+      </section> */}
 
       {/* Mobile navigation menu widgets */}
-      <section className="z-50 fixed flex flex-col gap-3 bottom-72 right-0">
+      <section className="z-50 hidden fixed lg:flex flex-col gap-3 bottom-72 right-0">
         <Link href="#home">
           <div className="font-medium p-3 pl-5 pr-5 bg-white text-black text-left border border-transparent hover:text-white hover:bg-black relative group hover:border-gradient items-center">
             <span className="transition-opacity opacity-100 group-hover:opacity-0 absolute flex items-center justify-center">
