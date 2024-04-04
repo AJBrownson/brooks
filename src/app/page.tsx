c066b0041446d230b955dc8104f6174aa210d61e
Anietie Brownson <anni.brownson@gmail.com>
2024-04-04 16:38:57 +0100

    feat: Add new tokenomics and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import HeroMobile from "../../public/hero-mobile.png";
 import About from "../../public/about-hero.png";
 import LockWhite from "../../public/lock-white.svg";
 import LockBlack from "../../public/lock-black.svg";
-import Circle from "../../public/circle.png";
+import Tokenomics from "../../public/tokenomics.png";
 import Gradient from "../../public/gradient-bg.svg";
 import { Carousel } from "@/components/Carousel";
 import Telegram from "../../public/Telegram.png";
@@ -25,17 +25,14 @@ import Logo from "../../public/logo.png";
 import NavMenu from "@/components/NavMenu";
 import Countdown from "@/components/Countdown";
 import Cancel from "../../public/cancel-01.png";
-import Roadmap from "../../public/roadmap-webb.png"
-import RoadmapMobile from "../../public/roadmap.png"
-import Drop from "../../public/drop.png"
-
+import Roadmap from "../../public/roadmap-webb.png";
+import RoadmapMobile from "../../public/roadmap.png";
+import Drop from "../../public/drop.png";
 
 const strokeStyle = {
   textShadow:
     "-1px -1px 0 #F5BF2D, 1px -1px 0 #F5BF2D, -1px 1px 0 #F5BF2D, 1px 1px 0 #F5BF2D",
 };
-
-
 
 export default function Home() {
   const [isDialogVisible, setIsDialogVisible] = useState(false);
@@ -60,13 +57,11 @@ export default function Home() {
         alt=""
         className="w-full object-cover lg:hidden"
       />
-
       {/* Mobile menu navigation  */}
       <div className="p-8 bg-blur fixed right-0 top-0 lg:hidden"></div>
       <section className="absolute top-1 right-8 lg:hidden p-4">
         <NavMenu />
       </section>
-
       {/* Gradient section lg:h-[21.5rem] absolute top-[9.5rem] md:top-72 lg:top-[12rem] */}
       {/* <section>
         <Image
@@ -75,15 +70,13 @@ export default function Home() {
           className="lg:h-[21.5rem] absolute top-32 md:top-72 lg:top-20 xl:top-[12rem] 2xl:top-32"
         />
       </section> */}
-
       <section className="gradientSection">
-      <Image
+        <Image
           src={Gradient}
           alt="A gradient picture"
           // className="lg:h-[21.5rem] absolute top-32 md:top-72 lg:top-20 xl:top-[12rem] 2xl:top-32"
         />
       </section>
-
       {/* Hero section top-[9.7rem] md:top-80 lg:top-56 : section=> top-32 md:top-80 lg:top-56 */}
       <section className="top-32 md:top-80 lg:top-40 xl:top-56 2xl:top-48 absolute z-10 text-center px-[6px] lg:px-0">
         <h1
@@ -100,7 +93,7 @@ export default function Home() {
 
         <div className="mt-2 flex lg:items-center justify-center gap-3">
           <button
-            onClick={openDialog}
+            // onClick={openDialog}
             className="mt-4 lg:mt-8 px-8 lg:px-12 py-2 lg:py-3 border border-[#3C3C3C] text-white relative group hover:border-gradient inline-flex items-center"
           >
             <span className="transition-opacity opacity-100 group-hover:opacity-0 absolute inset-0 flex items-center justify-center">
@@ -122,7 +115,7 @@ export default function Home() {
             </span>
           </button>
 
-           {/* <button
+          {/* <button
             onClick={openDialog}
             className="mt-4 lg:mt-8 px-20 lg:px-24 py-2 lg:py-3 border border-[#3C3C3C] text-white hover:border-white relative group hover:border-gradient inline-flex items-center"
           >
@@ -136,12 +129,10 @@ export default function Home() {
           </button>  */}
         </div>
       </section>
-
       {/* Countdown section py-20 mt-80 md:mt-96 */}
       {/* <section className="mt-60 md:mt-40 bg-presale bg-center bg-[length:60rem] md:bg-[length:120rem]">
         <Countdown />
       </section> */}
-
       {/* About section (countdown ? "mt-10 md:mt-0", mt-28 md:mt-80) : normal (mt-60 lg:mt-96 ) */}
       <section
         className="mt-60 lg:mt-96 px-2 lg:px-20 lg:pb-36 gap-10"
@@ -175,7 +166,6 @@ export default function Home() {
         </div>
         <Image src={VectorDown} alt="" className="mt-4 md:mt-16" />
       </section>
-
       {/* Story section */}
       <section className="mt-16 lg:mt-4 pb-4 py-10 px-[8px] lg:px-60 bg-story-gradient">
         <h1 className="text-[#00D5E9] font-orbitron text-center font-extrabold text-lg lg:text-2xl mb-5">
@@ -192,12 +182,10 @@ export default function Home() {
           that in the volatile realm of memes, quiet strength can reign supreme.
         </p>
       </section>
-
       {/* Carousel section */}
       <section>
         <Carousel />
       </section>
-
       {/* Gaming section gap-10 w-50% w-28 h-28*/}
       <section className="mt-20 lg:mt-36 px-2 lg:px-20">
         <div className="py-6 px-4 lg:px-8 flex justify-between gap-2 lg:gap-40 items-center bg-custom-gradient">
@@ -217,48 +205,106 @@ export default function Home() {
           <Image src={Gaming} alt="" className="w-40 h-40 lg:w-80 lg:h-80" />
         </div>
       </section>
-
       {/* Tokenomics section */}
-      {/* lg:justify-between justify-center rounded-sm  xl:max-w-lg lg:text-left */}
-      <section className="mt-20 px-4 lg:px-20 mb-20" id="tokenomics">
-        <div className="container flex flex-col mx-auto sm:pt-12 sm:pb-5 lg:pt-24 lg:pb-12 lg:flex-row justify-between">
-          <div className="flex flex-col justify-center lg:p-4 text-center lg:max-w-xl">
-            <h1 className="text-center leading-snug text-[28px] lg:text-5xl font-orbitron font-semibold">
+      {/* lg:justify-between justify-center rounded-sm  xl:max-w-lg lg:text-left */}{" "}
+      {/* className="hidden lg:flex items-center justify-center mt-8 lg:mt-0 lg:h-96" */}
+      <section className="mt-20 px-2 lg:px-20 mb-20" id="tokenomics">
+        <div className="">
+          <div className="flex flex-col justify-center lg:p-4 mb-8 md:mb-16 text-center">
+            <h1 className="text-center leading-snug tracking-wide md:tracking-normal text-[28px] lg:text-5xl font-orbitron font-semibold">
               Tokenomics
             </h1>
 
-            <div className="mb-5 lg:mb-0 mt-3 flex gap-2 justify-center">
-              <div className="p-1 bg-[#0099FF] h-2 w-2"></div>
-              <div className="p-1 bg-[#00CCFF] h-2 w-2"></div>
-              <div className="p-1 bg-[#1300FF] h-2 w-2"></div>
-              <div className="p-1 bg-[#6600FF] h-2 w-2"></div>
-              <div className="p-1 bg-[#9900FF] h-2 w-2"></div>
+            <div className="mb-5 lg:mb-0 mt-1 md:mt-3 flex gap-2 md:gap-3 justify-center">
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#996213] to-[#FFA41F] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#999902] to-[#FFFF04] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#2C9999] to-[#49FFFF] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#996468] to-[#FFA7AD] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#115A96] to-[#1D97FC] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#8AC45C] to-[#B3FF78] h-auto w-5 md:w-8"></div>
+              <div className="p-[2px] md:p-[3px] bg-gradient-to-r from-[#0D0D0D] via-[#7700C6] to-[#9900FF] h-auto w-5 md:w-8"></div>
             </div>
-
-            <p className="lg:hidden text-center mb-5 text-sm">
-              No taxes, no nonsense. It's as straightforward as that. LP tokens
-              are destroyed, and contract ownership is relinquished.
-            </p>
-
-            <div className="mb-10 lg:mb-0 lg:hidden flex items-center justify-center mt-4 lg:mt-0 h-72 sm:h-80 lg:h-96">
-              <Image src={Circle} alt="" className="w-[18rem] lg:w-full" />
-            </div>
-
-            <p className="hidden lg:block text-center mt-6 mb-2 text-lg">
-              No taxes, no nonsense. It's as straightforward as that. LP tokens
-              are destroyed, and contract ownership is relinquished.
-            </p>
           </div>
 
-          <div className="hidden lg:flex items-center justify-center mt-8 lg:mt-0 lg:h-96">
-            <Image src={Circle} alt="" className="W-20 " />
+          <div className="flex flex-col xl:flex-row items-center justify-between">
+            <div className="flex flex-col justify-center xl:-order-first">
+              <Image
+                src={Tokenomics}
+                alt=""
+                className="w-80 md:w-[35rem] h-auto"
+              />
+              <div className="text-center mt-5 flex items-center justify-center gap-4">
+                <p className="text-xs md:text-lg text-[#EDEDED] font-orbitron">
+                  Token supply:
+                </p>
+                <p className="text-[#F5F5F5] text-sm md:text-xl font-orbitron font-extrabold">
+                  11,999,999,000
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-12 xl:mt-0 flex flex-col gap-3">
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-20 md:w-44 bg-gradient-to-r from-[#996212] to-[#FFA41F]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">55% Presale</p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-10 md:w-20 bg-gradient-to-r from-[#999903] to-[#FFFF05]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">20% Presale</p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-1 md:w-2 bg-gradient-to-r from-[#2C9999] to-[#49FFFF]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">2% Team</p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-[6px] md:w-3 bg-gradient-to-r from-[#FFA7AD] to-[#996468]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">3% Airdrop</p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-5 md:w-10 bg-gradient-to-r from-[#1D96FF] to-[#115A99]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">
+                  10% CEX listing
+                </p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-[6px] md:w-3 bg-gradient-to-r from-[#B3FF78] to-[#6B9948]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">
+                  3% Marketing
+                </p>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-40 md:w-80 flex border border-[#3C3C3C]">
+                  <div className="w-[14px] md:w-7 bg-gradient-to-r from-[#9900FF] to-[#5C0099]"></div>
+                </div>
+                <p className="text-xs md:text-lg text-[#F2E6E0]">
+                  7% Games Improvements
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <Image src={VectorDown} alt="" />
+        <Image src={VectorDown} alt="" className="mt-5 lg:mt-10" />
       </section>
-
       {/* Roadmap section */}
-      <section className="px-3 md:px-5" id="roadmap">
+      <section className="px-2 md:px-5" id="roadmap">
         <h1 className="text-center text-[28px] lg:text-5xl font-orbitron font-semibold">
           Roadmap
         </h1>
@@ -272,11 +318,14 @@ export default function Home() {
           <Image src={Stages} alt="" className="mt-8 lg:hidden" />
         </div> */}
         <div className="mt-8 md:mt-14 flex items-center justify-center">
-          <Image src={Roadmap} alt="" className="hidden md:block md:w-[60rem] md:h-auto md:object-contain" />
+          <Image
+            src={Roadmap}
+            alt=""
+            className="hidden md:block md:w-[60rem] md:h-auto md:object-contain"
+          />
           <Image src={RoadmapMobile} alt="" className="md:hidden" />
         </div>
       </section>
-
       {/* Footer section */}
       <section className="text-center text-[#EDEDED] pb-5 px-[8px] lg:px-64">
         <Image src={VectorUp} alt="" className="pt-5 mb-4 lg:mb-16" />
@@ -287,7 +336,11 @@ export default function Home() {
           <Image src={Logo} alt="" className="w-20 h-20 xl:h-28 xl:w-28" />
         </div>
         <div className="flex justify-center gap-5 mb-6 lg:mb-14">
-          <Link href="https://discord.gg/3P6TmW5urD" rel="noopener noreferrer" target="_blank">
+          <Link
+            href="https://discord.gg/3P6TmW5urD"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <Image
               src={Discord}
               alt=""
@@ -301,7 +354,11 @@ export default function Home() {
               className="p-3 w-10 h-10 xl:w-14 xl:h-14 xl:p-4 border border-[#3C3C3C] group hover:border-gradient"
             />
           </Link>
-          <Link href="https://x.com/memerooks" rel="noopener noreferrer" target="_blank">
+          <Link
+            href="https://x.com/memerooks"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <Image
               src={X}
               alt=""
@@ -315,8 +372,7 @@ export default function Home() {
         <p className="text-sm lg:text-lg">
           $ROOKS is a lighthearted meme coin created purely for entertainment.
           It's not designed for making money or any financial gain. There's no
-          formal team behind it, just a fun token in the world of
-          memes.
+          formal team behind it, just a fun token in the world of memes.
         </p>
         <Image src={VectorDown} alt="" className="mt-5 lg:mt-16 lg:mb-3" />
 
@@ -326,7 +382,6 @@ export default function Home() {
           </p>
         </div>
       </section>
-
       {/* Desktop navigation menu widgets  xl:h-80 xl:pt-16 xl:top-0 */}
       <section className="bg-blur pl-4 pt-20 pb-3 z-50 hidden fixed lg:flex flex-col gap-1 bottom-[17rem] right-0 2xl:right-[80rem] xl:h-96 xl:pt-16 lg:top-0">
         <Link href="#home">
@@ -387,7 +442,11 @@ export default function Home() {
         </div>
 
         <div className="flex justify-between gap-3">
-          <Link href="https://discord.gg/3P6TmW5urD" rel="noopener noreferrer" target="_blank">
+          <Link
+            href="https://discord.gg/3P6TmW5urD"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <div className="bg-white hover:bg-black border border-black relative group p-5 group">
               <Image
                 src={DiscordDark}
@@ -415,7 +474,11 @@ export default function Home() {
               />
             </div>
           </Link>
-          <Link href="https://x.com/memerooks" rel="noopener noreferrer" target="_blank">
+          <Link
+            href="https://x.com/memerooks"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <div className="bg-white hover:bg-black border border-black relative group p-5 group">
               <Image
                 src={XDark}
@@ -431,7 +494,6 @@ export default function Home() {
           </Link>
         </div>
       </section>
-
       {/* Dialog modal section */}
       <ModalDialog
         isDialogVisible={isDialogVisible}
@@ -448,7 +510,11 @@ export default function Home() {
               className="font-orbitron text-xl font-thin hover:font-semibold absolute top-6 lg:top-6 right-2 lg:right-4"
               onClick={closeDialog}
             >
-              <Image src={Cancel} alt="" className="hover:border border-gray-500 rounded-md" />
+              <Image
+                src={Cancel}
+                alt=""
+                className="hover:border border-gray-500 rounded-md"
+              />
             </button>
           </div>
 
